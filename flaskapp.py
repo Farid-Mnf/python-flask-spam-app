b91466fec9e47ae29971c6c199fb46a93aac5baf
Farid <faridggl100@gmail.com>
2021-03-22 03:51:40 +0200

    html page get user input and check spam or not with ajax request to flask /spam api endpoint

--- flaskapp.py
+++ flaskapp.py
@@ -3,6 +3,11 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+from flask import render_template
+
+@app.route("/")
+def index():
+    return render_template("index.html")
 
 @app.route('/spam/<string:email>')
 def hello(email):
